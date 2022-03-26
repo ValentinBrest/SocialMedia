133a19fa45834ce75b427873921a12db05b390b4
Valiantsin Kanapatski <79157063+ValentinBrest@users.noreply.github.com>
2022-03-27 01:20:26 +0300

    crate profilePage

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Preloader from '../../UI/Preloader/Preloader';
+import cl from './../Profile.module.css'
+
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
+    return (
+        <div>
+            <div className={cl.about}>
+                    <img src={props.profile.photos.large} alt="ava" className="about__avatar" />
+                    <div className={cl.about__descr}>
+                        <h1>{props.profile.fullName}</h1>
+                        <hr />
+                        <div className={cl.about__text}>
+                            <div className={cl.about__item}>About me: <span className={cl.status}> {props.profile.aboutMe}</span></div>
+                            <div className={cl.about__item}>Contacts: 
+                                    <div className={cl.social}>
+                                        <NavLink to={props.profile.contacts.facebook} className={cl.social_link}>facebook</NavLink>
+                                        <NavLink to={props.profile.contacts.vk} className={cl.social_link}>vk</NavLink>
+                                        <NavLink to={props.profile.contacts.instagram} className={cl.social_link}>instagram</NavLink>
+                                    </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+        </div>
+    );
+};
+
+export default ProfileInfo;
